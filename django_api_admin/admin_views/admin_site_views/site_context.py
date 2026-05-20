b9b1bcfc814c22eeebd67841c50f9fba83d8e325
daemon-bixia <msmainacc0unt@gmail.com>
2026-05-20 14:32:52 +0400

    refactor: Update openapi documentation for `SiteContextView`

--- django_api_admin/admin_views/admin_site_views/site_context.py
+++ django_api_admin/admin_views/admin_site_views/site_context.py
@@ -1,11 +1,10 @@
-# from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from drf_spectacular.utils import extend_schema
-
+from drf_spectacular.utils import extend_schema, OpenApiResponse, OpenApiExample
 
 from django_api_admin.serializers import SiteContextSerializer
 from django_api_admin.openapi import CommonAPIResponses
@@ -13,14 +12,57 @@ from django_api_admin.openapi import CommonAPIResponses
 
 class SiteContextView(APIView):
     """
-    Returns the Attributes of AdminSite class (e.g. site_title, site_header)
+    Retrieve core attributes of the admin site.
+
+    Returns a JSON payload containing configurable admin site settings such as
+    `site_title`, `site_header`, `site_url`, and registered apps in the admin 
+    site.
     """
+
     permission_classes = []
     admin_site = None
 
     @extend_schema(
+        operation_id="Retrieve admin site context",
         responses={
-            200: SiteContextSerializer,
+            200: OpenApiResponse(
+                response=SiteContextSerializer,
+                description=_(
+                    "Retrieve site context attributes such as site title and header"),
+                examples=[
+                    OpenApiExample(
+                        name=_("Success Response"),
+                        value={
+                            "site_title": "Django site admin",
+                            "site_header": "Django administration",
+                            "site_url": "/",
+                            "has_permission": True,
+                            "available_apps": [
+                                {
+                                    "name": "Authentication and Authorization",
+                                    "app_label": "auth",
+                                    "app_url": "/api_admin/auth/",
+                                    "has_module_perms": True,
+                                    "models": [
+                                        {
+                                            "name": "Users",
+                                            "object_name": "User",
+                                            "perms": {
+                                                "add": True,
+                                                "change": True,
+                                                "delete": True,
+                                                "view": True
+                                            }
+                                        }
+                                    ]
+                                }
+                            ],
+                            "is_nav_sidebar_enabled": True
+                        },
+                        status_codes=["200"]
+                    )
+                ],
+            ),
             403: CommonAPIResponses.permission_denied(),
             401: CommonAPIResponses.unauthorized()
         },
