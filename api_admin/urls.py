2f2ea1313df55e6454e163918f34de94891c2b3e
daemon-bixia <msmainacc0unt@gmail.com>
2026-06-02 18:46:13 +0400

    feat: Add `example` app

--- api_admin/urls.py
+++ api_admin/urls.py
@@ -1,20 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from test_django_api_admin import views
-from test_django_api_admin.admin import site
+from example.admin import site
 
 urlpatterns = [
     # Both the api admin and the default admin
     path("admin/", admin.site.urls),
     path("api_admin/", site.urls),
-    path("api/author/<int:pk>/",
-         views.AuthorDetailView.as_view(), name="author-detail"),
-    path("api/publisher/<int:pk>/",
-         views.PublisherDetailView.as_view(), name="publisher-detail"),
     # The admin panel uses the same authentication backend as the application
     path("_allauth/", include("allauth.headless.urls")),
     path("accounts/", include("allauth.urls")),
-    # Test your form fields.
-    # path('api_admin/field_tests/<str:test_name>/', views.TestView.as_view()),
 ]
---
+++ example/actions.py
@@ -0,0 +1,15 @@
+from django.utils.translation import gettext_lazy as _
+
+from rest_framework import status
+from rest_framework.response import Response
+
+from django_api_admin.decorators import action
+
+
+@action(description=_("Mark selected products as out of stock"))
+def mark_out_of_stock(modeladmin, request, queryset):
+    updated = queryset.update(stock_status="out_of_stock")
+    return Response(
+        {"detail": _("Successfully marked %d products as out of stock.") % updated},
+        status=status.HTTP_200_OK
+    )
